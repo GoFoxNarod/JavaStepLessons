f1a3e6c6cfb09c37c08766871e91875aff19f097
GoFoxNarod <go.fox.narod@gmail.com>
2023-11-09 16:07:05 +0300

    Update Animal.java

--- src/Animal.java
+++ src/Animal.java
@@ -1,3 +1,50 @@
-class Animal {
-  
+public class Animal {
+
+    String type;
+     String name;
+    int age;
+    double weight;
+    boolean isFly;
+    boolean isWalk;
+    boolean isSwim;
+
+    public Animal(){
+
+    }
+
+    public Animal(String type,String name, int age, double weight, boolean isFly, boolean isSwim, boolean isWalk){
+        this.type = type;
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.isFly = isFly;
+        this.isSwim = isSwim;
+        this.isWalk = isWalk;
+
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void display() {
+        System.out.println("Тип: " + type + ", Имя: " + name + ", Возраст: " + age + ", Вес: " + weight +
+                ", Умеет летать: " + (isFly ? "Да" : "Нет") + ", Умеет ходить: "
+                + (isWalk ? "Да" : "Нет") + ", Умеет плавать: " + (isSwim ? "Да" : "Нет"));
+    }
+
+
+    public void holiday(int n) {
+        weight += n * 0.1;
+    }
+    public void holiday(double m, int n) {
+        weight += n * m;
+        weight = Math.ceil(weight * 10) / 10;
+    }
+    public void holiday(double m) {
+        holiday(m, 1);
+    }
+    public void holiday() {
+        holiday(0.1);
+    }
 }
