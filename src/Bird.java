3f4724a1989348d405fc9973e58e17ed166ef320
GoFoxNarod <go.fox.narod@gmail.com>
2023-11-09 16:08:21 +0300

    Update Bird.java

--- src/Bird.java
+++ src/Bird.java
@@ -1,1 +1,42 @@
+public class Bird extends Animal {
+    String area;
+    boolean winterFly;
+    public Bird(){
+        super();
+        super.type = "Птица";
+        super.isFly = true;
+        this.area = "No area";
+        this.winterFly = false;
+
+    }
+
+    public void setName(String name){
+        super.name = name;
+    }
+
+    public void setArea(String area){
+
+        this.area = area;
+    }
+
+    public String getArea(){
+
+        return "Зона обитания :" +area+".";
+    }
+
+    public void setWinterFly(boolean winterFly){
+
+        this.winterFly = winterFly;
+    }
+
+    public String  isWinterFly(){
+
+        return "Улетают зимовать:" + (winterFly ? "ДА" : "НЕТ" )+".";
+    }
+
+
+    public void chirik_chirik(){
+        System.out.println("Chirik-Chirik");
+    }
+}
 
