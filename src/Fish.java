e66392945184de1fcbfd61c79593e5ee526fa510
GoFoxNarod <go.fox.narod@gmail.com>
2023-11-09 16:11:20 +0300

    Update Fish.java

--- src/Fish.java
+++ src/Fish.java
@@ -1,1 +1,36 @@
+public class Fish extends Animal{
+    String squama;
+    boolean upStreamSwim;
+
+    Fish(){
+        super();
+        super.type = "РЫБЫ";
+        super.isSwim = true;
+        this.squama = "No";
+        this.upStreamSwim = false;
+
+    }
+
+    public void setName(String name){
+        super.name = name;
+    }
+
+    public void setSquama(String squama){
+        this.squama = squama;
+    }
+
+    public String getSquama(){
+        return "Размер чешуи:"+squama+".";
+    }
+    public void setUpStreamSwim(boolean upStreamSwim){
+        this.upStreamSwim = upStreamSwim;
+    }
+
+    public String isUpStreamSwim(){
+        return "Умеет ли плавать против течения:"+(upStreamSwim ? "ДА":"НЕТ");
+    }
+    public void bul_bul(){
+        System.out.println("BUL-BUL");
+    }
+}
 
