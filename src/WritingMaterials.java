5a8b3c990d6738cc4b45abc97ffbc60c5e917532
Alex <go.fox.narod@gmail.com>
2023-11-13 15:40:20 +0300

    Добавили переменные,конструктор,методы в класс WritingMaterials.java

--- src/WritingMaterials.java
+++ src/WritingMaterials.java
@@ -1,2 +1,29 @@
 public class WritingMaterials {
+    String name;
+    String color;
+    int price;
+    double length;
+    boolean draw;
+
+    WritingMaterials(){
+
+    }
+    WritingMaterials(String name, String color, int price,double length, boolean draw){
+        this.name = name;
+        this.color = color;
+        this.price = price;
+        this.length = length;
+        this.draw = draw;
+    }
+
+    public void display(){
+        System.out.println("Название:"+ name +". Цвет:"+ color +". Длина:"+ price+ ". Умеет рисовать:"+ (draw ?"ДА":"НЕТ"));
+    }
+    public void replace_rod(String color){
+        this.color = color;
+    }
+    public void priceUp(int price){
+        this.price = price;
+    }
 }
+
