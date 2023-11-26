3fbe578d0405d80dfb2c0659536fb98937afe45f
admin <go.fox.narod@gmail.com>
2023-11-26 19:57:12 +0300

    -

--- src/Ruler.java
+++ src/Ruler.java
@@ -1,2 +1,26 @@
 public class Ruler extends WritingMaterials{
+    double length;
+    boolean wood;
+    public Ruler(){
+        super();
+        super.setDraw(false);
+    }
+
+    public Ruler(String name, String color, int price, double length, boolean draw, double length1, boolean wood) {
+        super(name, color, price, length, draw);
+        this.length = length1;
+        this.wood = wood;
+    }
+
+    public Ruler(String name, int price, double length, boolean draw, double length1, boolean wood) {
+        super(name, price, length, draw);
+        this.length = length1;
+        this.wood = wood;
+    }
+
+    public Ruler(String name, int price, double length, boolean wood) {
+        super(name, price);
+        this.length = length;
+        this.wood = wood;
+    }
 }
